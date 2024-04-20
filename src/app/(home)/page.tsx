1c58a5b800563b2a9c6e55947c22ac188122476f
Gaurav <gouraw1@gmail.com>
2024-04-20 13:07:12 +0530

    resolve error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,12 +9,12 @@ import Experience from "./components/Experience";
 const page = () => {
   return (
     <div className="min-h-screen overflow-hidden">
-      <div className="relative  dark:bg-stone-950 bg-white dark:bg-grid-white/[0.02] bg-grid-black/[0.02]">
+      <div className="relative  dark:bg-stone-950 z-10 bg-white dark:bg-grid-white/[0.04] bg-grid-black/[0.04]">
         <div className="max-w-7xl mx-auto p-5">
           <Navbar />
           <HeroSection />
         </div>
-        <div className="h-10 xl:h-32 bg-gradient-to-t from-white  dark:bg-gradient-to-t dark:from-stone-950 absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
+        <div className="h-10 xl:h-32 bg-gradient-to-t from-white  dark:bg-gradient-to-t dark:from-stone-950 absolute -bottom-5 left-0 xl:bottom-0 w-full -z-30"></div>
       </div>
 
       <div className="max-w-7xl mx-auto p-5 mt-20">
